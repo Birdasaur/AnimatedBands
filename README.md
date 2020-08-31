5aec873a9e8f563b8c26a44dc117995d35fdf823
Carl Dea <carldea@gmail.com>
2020-08-30 22:44:10 -0400

    Converted single abstract methods to Lambda expressions. Added instructions to run app

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ JavaFX 14 (OpenJFX)
 
 Demonstrates the generation of animated bands based on points of randomized length (gaussian) along an arc around a centroid (mouse click). Points of band are animated
 using a Timeline to move away from the centroid. Band is rendered using a Path which generates QuadCurves using the points as control points for a smooth fit. This is inspired by the old JavaFX 1.x Ripple generator which only animated simple circles.
+
+```bash
+$ mvn clean javafx:run
+```
--- src/main/java/animatedbands/App.java
+++ src/main/java/animatedbands/App.java
@@ -1,17 +1,10 @@
 package animatedbands;
 
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.collections.FXCollections;
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.Paint;
-import javafx.scene.paint.RadialGradient;
-import javafx.scene.paint.Stop;
+import javafx.scene.Scene;
+import javafx.scene.paint.*;
+import javafx.stage.Stage;
 
 public class App extends Application {
 
@@ -24,33 +17,22 @@ public class App extends Application {
     public void start(Stage stage) {
         BandGenerator bands = new BandGenerator(50, 100, 20.0);
         final Scene scene = new Scene(bands, 600, 600, SCENE_FILL);
-        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if(event.isSecondaryButtonDown()) {
-                    bands.getChildren().clear();
-                } else if(event.isPrimaryButtonDown()) {
-                    bands.setGeneratorCenterX(event.getSceneX());
-                    bands.setGeneratorCenterY(event.getSceneY());
-                    bands.createQuadBand();
-                }
+        scene.setOnMousePressed( mouseEvent -> {
+            if(mouseEvent.isSecondaryButtonDown()) {
+                bands.getChildren().clear();
+            } else if(mouseEvent.isPrimaryButtonDown()) {
+                bands.setGeneratorCenterX(mouseEvent.getSceneX());
+                bands.setGeneratorCenterY(mouseEvent.getSceneY());
+                bands.createQuadBand();
             }
         });
 
-        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                bands.setGeneratorCenterX(event.getSceneX());
-                bands.setGeneratorCenterY(event.getSceneY());
-            }
+        scene.setOnMouseDragged(mouseEvent -> {
+            bands.setGeneratorCenterX(mouseEvent.getSceneX());
+            bands.setGeneratorCenterY(mouseEvent.getSceneY());
         });
 
-        scene.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                bands.stopGenerating();
-            }
-        });
+        scene.setOnMouseReleased(mouseEvent -> bands.stopGenerating());
 
         stage.setTitle("Click to create random band. Rightclick to clear.");
         stage.setScene(scene);
